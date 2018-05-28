16d704c699cbc8334ea8b41c4511c1cd1a1a11ab
VincentCung <326216746@qq.com>
2018-05-28 22:36:09 +0800

    save

--- client/Components/delete-list/delete-list.js
+++ client/Components/delete-list/delete-list.js
@@ -4,9 +4,9 @@ Component({
         configList: {
             type: Array,
         },
-        bubbleTap:{
+        bubbleTap: {
             type: Boolean,
-            value:false
+            value: false
         }
     },
     options: {
@@ -16,18 +16,17 @@ Component({
     data: {
         startX: 0,//滑动开始的x坐标
         buttonWidth: 100,//删除按钮的宽
+        activeIndex: NaN
     },
 
     methods: {
         ontouchStart(e) {
-            let currentIndex = e.currentTarget.dataset.index;
             if (e.touches.length == 1) {
-                let configList = this.data.configList;
-                configList.map((value, index) => {
-                    if (index != currentIndex) {
-                        value.leftDistance = 0;
-                    } return value;
-                });
+                let currentIndex = e.currentTarget.dataset.index;
+                let { configList, activeIndex } = this.data;
+                if(activeIndex){
+                    configList[activeIndex].leftDistance = 0;
+                }
                 this.setData({
                     startX: e.touches[0].clientX
                 });
@@ -35,37 +34,25 @@ Component({
 
         },
 
-        ontouchMove(e) {
-            let that = this;
-            if (e.touches.length == 1) {
-                let leftDistance= 0
-                let moveX = e.touches[0].clientX;
-                let distance = this.data.startX - moveX;
-                let index = e.currentTarget.dataset.index;  //获取手指触摸的是哪一项
-                let { configList, buttonWidth } = this.data
-                if (distance <= 0) {                    //如果移动距离小于等于0，文本层位置不变
-                    leftDistance = 0
-                } else {                        //移动距离大于0，文本层left值等于手指移动距离
-                    leftDistance = (distance < buttonWidth ? -distance : -buttonWidth)
-                    //控制手指移动距离最大值为删除按钮的宽度
-                }
-                configList[index].leftDistance = leftDistance;
-                this.setData({
-                    configList
-                });
-            }
-        },
 
         ontouchEnd(e) {
             if (e.changedTouches.length == 1) {
                 let moveX = e.changedTouches[0].clientX;
                 let distance = this.data.startX - moveX;
                 let index = e.currentTarget.dataset.index;
-                let { buttonWidth, configList } = this.data
-                configList[index].leftDistance = (distance > buttonWidth / 2 ? -buttonWidth : 0)
+                let { buttonWidth, configList, activeIndex } = this.data
+                if (distance > buttonWidth / 2) {
+                    configList[index].leftDistance = -buttonWidth;
+                    activeIndex = index;
+                } else {
+                    configList[index].leftDistance = 0
+                    activeIndex = NaN;
+                }
                 this.setData({
-                    configList
+                    configList,
+                    activeIndex
                 })
+                console.log(e)
             }
         },
 
@@ -85,7 +72,6 @@ Component({
             this.triggerEvent('deleteItem', { itemId }, eventOption)
         },
         onTap(e) {
-            let configList = this.data.configList;
             let index = e.currentTarget.dataset.index;
             if (configList[index].leftDistance == 0) {
                 let eventOption = {
