278e705222f1872f53214601866cd733b28f3f7e
VincentCung <326216746@qq.com>
2018-05-31 22:32:41 +0800

    update map

--- client/pages/newMap/newMap.js
+++ client/pages/newMap/newMap.js
@@ -1,25 +1,57 @@
 // pages/newMap/newMap.js
 
 const config = require('../../config')
+const app = getApp()
 Page({
     /**
      * 页面的初始数据
      */
     data: {
-        mapid:null,
-        location:["北京市","北京市","东城区"],
-        textareaLen:0,
-        textareaMaxLen:50,
-        tags: [
-            { tagName: "日韩料理", isChoosed: false },
-            { tagName: "西式简餐", isChoosed: false },
-            { tagName: "川湘菜", isChoosed: false },
-            { tagName: "东南亚风情", isChoosed: false },
-            { tagName: "下午茶甜点", isChoosed: false },
-            { tagName: "特色私房", isChoosed: false },
-            { tagName: "养生早点", isChoosed: false },
-            { tagName: "简约小食", isChoosed: false },
-            { tagName: "其他", isChoosed: false }],
+        mapid: null,
+        location: ["北京市", "北京市", "东城区"],
+        mapName: '',
+        description: '',
+        isPublic: true,
+        textareaLen: 0,
+        textareaMaxLen: 50,
+        tags: [{
+                tagName: "日韩料理",
+                isChoosed: false
+            },
+            {
+                tagName: "西式简餐",
+                isChoosed: false
+            },
+            {
+                tagName: "川湘菜",
+                isChoosed: false
+            },
+            {
+                tagName: "东南亚风情",
+                isChoosed: false
+            },
+            {
+                tagName: "下午茶甜点",
+                isChoosed: false
+            },
+            {
+                tagName: "特色私房",
+                isChoosed: false
+            },
+            {
+                tagName: "养生早点",
+                isChoosed: false
+            },
+            {
+                tagName: "简约小食",
+                isChoosed: false
+            },
+            {
+                tagName: "其他",
+                isChoosed: false
+            }
+        ],
+
     },
     toggleTagState: function (e) {
         let obj = {}
@@ -35,39 +67,24 @@ Page({
      */
     formSubmit: function (e) {
         let newMap = e.detail.value
-        let openId=getApp().data.userInfo.openId
-        if(newMap.mapName=='') {
+        console.log(newMap)
+        if (newMap.mapName == '') {
             wx.showModal({
                 title: '提示',
                 content: '地图名不能为空',
-                showCancel:false,
-                confirmColor:'#EB6159'
+                showCancel: false,
+                confirmColor: '#EB6159'
             })
         } else {
-            let data = {
-                open_id: openId,
-                map: {
-                    map_name: newMap.mapName,
-                    description: newMap.description,
-                    province: this.data.location[0],
-                    city: this.data.location[1],
-                    locality: this.data.location[2],
-                    is_public: newMap.isPublic
-                }
-            };
-            console.log(data)
-            //向后台发送数据
-            wx.request({
-                url: config.service.host + "/map/myMap",
-                method: "POST",
-                data,
-                success: function (res) {
-                    console.log(res)
-                }
 
-            })
+            if (this.data.mapid) {
+                this._updateRequest(e.detail.value)
+            } else {
+                this._createRequest(e.detail.value)
+            }
+
         }
-        
+
     },
     /**
      * picker发生改变
@@ -81,22 +98,26 @@ Page({
     /**
      * textarea实时更新字数
      */
-    bindTextAreaChange:function(e) {
+    bindTextAreaChange: function (e) {
         var that = this
         var len = parseInt(e.detail.value.length)
-        that.setData({ textareaLen: len })
+        that.setData({
+            textareaLen: len
+        })
     },
 
     onLoad() {
-        if (this.options.mapid) { //更新坐标
+        if (this.options.mapid) { //更新地图
             let mapid = this.options.mapid
             wx.setNavigationBarTitle({
                 title: '管理地图信息',
             })
-            this.setData({ mapid })
+            this.setData({
+                mapid
+            })
             this._setLoadDetail()
-        } else { //新建坐标
-            
+        } else { //新建地图
+
         }
     },
 
@@ -116,16 +137,97 @@ Page({
     },
 
     _dataProcess(rawData) {
-        let categories = this.data.categories
-        let categoryIndex = categories.indexOf(rawData.category)
-
+        console.log(rawData)
         let data = {
-            map_name: rawData.map_name,
+            mapName: rawData.map_name,
+            location: [rawData.province, rawData.city, rawData.locality],
             description: rawData.description,
-            categoryIndex,
-            textareaLen: rawData.description.length
+            textareaLen: rawData.description.length,
+            isPublic: !!rawData.is_public
         }
+        let index;
+        let tags = this.data.tags
+        for (let category of rawData.category) {
+            console.log(category)
+            index = tags.findIndex((v, i) => v.tagName == category)
+            if (index != -1) {
+                console.log(tags)
+                console.log(index)
+                tags[index].isChoosed = true
+            }
 
+        }
+        data.tags = tags
         return data
+    },
+    _createRequest(rawData) {
+        let category = []
+        let categories = this.data.tags
+        for (let item of categories) {
+            if (item.isChoosed) {
+                category.push(item.tagName)
+            }
+        }
+        let data = {
+            open_id: app.data.userInfo.openId,
+            map: {
+                map_name: rawData.mapName,
+                description: rawData.description,
+                province: rawData.location[0],
+                city: rawData.location[1],
+                locality: rawData.location[2],
+                is_public: rawData.isPublic,
+                category
+            }
+        };
+        this._dataRequest(data, 0)
+
+    },
+
+    _updateRequest(rawData) {
+        let category = []
+        let categories = this.data.tags
+        for (let item of categories) {
+            if (item.isChoosed) {
+                category.push(item.tagName)
+            }
+        }
+        let map = {
+            map_name: rawData.mapName,
+            description: rawData.description,
+            province: rawData.location[0],
+            city: rawData.location[1],
+            locality: rawData.location[2],
+            is_public: rawData.isPublic,
+            category
+        }
+        let data = {
+            mapid:this.data.mapid,
+            map
+        }
+        this._dataRequest(data, 1)
+
+    },
+
+
+    _dataRequest(data, choice) {
+        let choices = ['POST', 'PUT']
+        let method = choices[choice]
+        console.log(data)
+        wx.request({
+            url: config.service.host + '/map/myMap',
+            method,
+            data,
+            success() {
+                wx.showToast({
+                    title: choice == 0 ? '创建成功' : '保存成功',
+                    icon: 'success',
+                })
+                wx.navigateBack({
+
+                })
+            }
+        })
+    
     }
 })
